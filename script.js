11023e80d7db51a91cd8d1208a7a1511bc121e3d
Jorismin <95677161+Jorismin@users.noreply.github.com>
2022-07-18 15:26:32 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+//Menu Burger Navbar
+const burgerContainer = document.querySelector('.burger-container');
+const navLinks = document.querySelector('.nav-links');
+
+burgerContainer.addEventListener('click', () => {
+    burgerContainer.classList.toggle('active');
+    navLinks.classList.toggle('active');
+})
+// SLIDER SLIDER SLIDER SLIDER SLIDER SLIDER SLIDER SLIDER SLIDER SLIDER SLIDER SLIDER SLIDER SLIDER   
+let img__slider = document.getElementsByClassName('img__slider');
+
+let etape = 0;
+
+let nbr__img = img__slider.length;
+
+let precedent = document.querySelector('.precedent');
+let suivant = document.querySelector('.suivant');
+
+function enleverActiveImages() {
+    for(let i = 0 ; i < nbr__img ; i++) {
+        img__slider[i].classList.remove('active');
+    }
+}
+
+suivant.addEventListener('click', function() {
+    etape++;
+    if(etape >= nbr__img) {
+        etape = 0;
+    }
+    enleverActiveImages();
+    img__slider[etape].classList.add('active');
+})
+
+precedent.addEventListener('click', function() {
+    etape--;
+    if(etape < 0) {
+        etape = nbr__img - 1;
+    }
+    enleverActiveImages();
+    img__slider[etape].classList.add('active');
+})
+
+setInterval(function() {
+    etape++;
+    if(etape >= nbr__img) {
+        etape = 0;
+    }
+    enleverActiveImages();
+    img__slider[etape].classList.add('active');
+}, 5000)
+
+
